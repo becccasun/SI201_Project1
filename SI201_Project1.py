2f433f22434e9c63d3674615e51134f5f91d5d60
Rebecca Sun <rebeccasun@Rebeccas-MacBook-Air-55.local>
2025-10-06 12:43:28 -0400

    Edit 1

---
+++ SI201_Project1.py
@@ -0,0 +1,14 @@
+# SI 201 Project 1
+# Rebecca Sun, working wit Junjin Tan, Anu Narayan
+# 10/06/2025
+# 18852123
+# beccasun@umich.edu
+
+import csv
+with open('penguins.csv', 'r') as file:
+    reader = csv.ready(file)
+    header = next(reader)
+
+# number of adelie penguins
+# number of female penguins
+
