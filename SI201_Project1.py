f28114fa02a39934393e92b6e926147c6b94190e
Rebecca Sun <rebeccasun@Rebeccas-MacBook-Air-55.local>
2025-10-06 21:37:41 -0400

    Edit 2

--- SI201_Project1.py
+++ SI201_Project1.py
@@ -5,10 +5,23 @@
 # beccasun@umich.edu
 
 import csv
-with open('penguins.csv', 'r') as file:
-    reader = csv.ready(file)
-    header = next(reader)
+
+with open('penguins.csv', newline="") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
 
 # number of adelie penguins
 # number of female penguins
 
+def analyze_penguins(csv_path: str, out_path: str = "penguin_results.txt") -> None:
+    #Read penguin.csv, analyze data, and writes a report to out_path
+    total_adelie = 0
+    total_female = 0
+
+    year_sum = 0
+    year_count = 0
+
+    with open(csv_path, newline ="") as csvfile:
+        reader = csv.reader(csvfile)
+        # % of adelie penguins
